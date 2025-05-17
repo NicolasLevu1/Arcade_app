473755a09536ba3699d5af01d822a83daef07ac5
NaN0987 <116316543+NaN0987@users.noreply.github.com>
2025-05-17 11:38:17 -0400

    temp changes (please undo later)
    These changes allow us to test frontend stuff on windows

--- arcade_machine/arcade_server.py
+++ arcade_machine/arcade_server.py
@@ -38,18 +38,19 @@ def serve_images(filename):
 # Return current game (new endpoint)
 @app.route('/current_game')
 def get_current_game():
-    with open("/opt/retropie/configs/all/autostart.sh", "r") as file:
-        data = file.read()
+    # with open("/opt/retropie/configs/all/autostart.sh", "r") as file:
+    #     data = file.read()
 
-    try: 
-        game_path = data.strip()
-        game_path = game_path.split("/")[-1]
-        current_game = game_path
+    # try: 
+    #     game_path = data.strip()
+    #     game_path = game_path.split("/")[-1]
+    #     current_game = game_path
 
-        return jsonify({"current_game": current_game})
+    #     return jsonify({"current_game": current_game})
     
-    except:
-        return jsonify({"current_game": "No current game"})
+    # except:
+    #     return jsonify({"current_game": "No current game"})
+    return jsonify({"current_game": "Pac-Man"})
 
 # Set current game name (for internal tools or UI call)
 @app.route('/set_game', methods=['POST'])
@@ -68,9 +69,25 @@ def stats():
     # The python script controlling the coin slot will track how many coins are inserted
     # for each game in a json file. This function can just return that json file
     # and the browser can use it to create a chart or bar graph.
-    with open("/home/pi/coinslot/coin_info.json", "r") as file:
-        data = file.read()
-    return data
+    # with open("/home/pi/coinslot/coin_info.json", "r") as file:
+    #     data = file.read()
+    # return data
+    return """
+    {
+        "Pac-Man": {
+            "coins": [3, 2, 5],
+            "times": ["2025-05-10 14:30", "2025-05-11 12:15", "2025-05-12 16:45"]
+        },
+        "Galaga": {
+            "coins": [1, 4, 2],
+            "times": ["2025-05-10 13:00", "2025-05-11 14:45", "2025-05-13 18:30"]
+        },
+        "Donkey Kong": {
+            "coins": [2, 1, 3, 4],
+            "times": ["2025-05-09 11:20", "2025-05-10 15:50", "2025-05-12 17:10", "2025-05-14 20:00"]
+        }
+    }
+    """
 
 # Reset everything
 @app.route('/reset_stats', methods=['POST'])
